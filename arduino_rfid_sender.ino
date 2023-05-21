c150e6abc09829524b8aa3f9552c329ed2613ecf
MakerZip <makerzip@hanmail.net>
2023-05-21 16:37:37 +0900

    코드 추가

---
+++ arduino_rfid_sender.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() { 
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+  Serial.println("OK");
+}
+ 
+void loop() {
+  if(!rfid.PICC_IsNewCardPresent()){
+    return;
+  }
+  if(!rfid.PICC_ReadCardSerial()){
+    return;
+  }
+  String content="";
+  byte letter;
+  for (byte i = 0; i < rfid.uid.size; i++){
+    content.concat(String(rfid.uid.uidByte[i] < 0x10?" 0":" "));
+    content.concat(String(rfid.uid.uidByte[i], HEX));
+  }
+  content = content.substring(1);
+  content.toUpperCase();
+  Serial.println(content);
+  delay(3000);
+}
