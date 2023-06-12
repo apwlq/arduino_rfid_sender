6f89ca8c6e1c2c8cf2aa877ec6ada965fd363a51
MakerZip <makerzip@hanmail.net>
2023-06-12 11:09:39 +0900

    소소한 버그 수정

--- arduino_rfid_sender.ino
+++ arduino_rfid_sender.ino
@@ -4,13 +4,13 @@
 #define RST_PIN 9
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-void setup() { 
+void setup() {
   Serial.begin(9600);
   SPI.begin();
   rfid.PCD_Init();
   Serial.println("OK");
 }
- 
+
 void loop() {
   if (Serial.available() > 0) {
     String input = Serial.readString();
@@ -19,19 +19,20 @@ void loop() {
       Serial.println("PONG");
     }
   }
-  if(!rfid.PICC_IsNewCardPresent()) {
+
+  if (!rfid.PICC_IsNewCardPresent()) {
     return;
   }
-  if(!rfid.PICC_ReadCardSerial()) {
+
+  if (!rfid.PICC_ReadCardSerial()) {
     return;
   }
-  String content="";
-  byte letter;
+
+  String content = "";
   for (byte i = 0; i < rfid.uid.size; i++) {
-    content.concat(String(rfid.uid.uidByte[i] < 0x10?" 0":" "));
+    content.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
     content.concat(String(rfid.uid.uidByte[i], HEX));
   }
-  content = content.substring(1);
   content.toUpperCase();
   Serial.println(content);
   delay(1000);
